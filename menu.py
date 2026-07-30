f686df1bd5790ececa104bf56f3c00deb9821878
Tristan McCrea <papijackwaiter@gmail.com>
2026-07-30 08:40:12 -0400

    Update menu.py
    Add a simple menu display and run functionality. handle_input will deal with input validation, as well as calling data_modifier methods

--- menu.py
+++ menu.py
@@ -7,10 +7,28 @@ class Menu:
 
     def run(self):
         # Main loop: print the menu, get input, handle w functions below, repeat until quit
-        pass
+        while True:
+            self.printmenu()
+            choice = input("Enter your choice: ")
+
+            if choice == "8":
+                print("Goodbye!")
+                break
+
+            self.handle_input(choice)
 
     def print_menu(self):
         # Display available options to the user
+        print("\n--- Data Modifier Menu ---")
+        print("1) Reset to original data")
+        print("2) Sort")
+        print("3) Filter")
+        print("4) Aggregate (mean/median/sum)")
+        print("5) Correlate")
+        print("6) Display current data")
+        print("7) Show columns")
+        print("8) Quit")
+        # Loop the default menu. Have multiple options for the user (reset, sort, filter, aggregate, correlate, display, quit)
         pass
 
     def handle_input(self, choice):
